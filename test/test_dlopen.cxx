3bc8568b1d077ef217f31ce42f2d89e503d63b0c
Brett Viren <brett.viren@gmail.com>
2015-10-09 14:52:09 -0400

    Fix up some misconceptions about how templated singletons work.

--- test/test_dlopen.cxx
+++ test/test_dlopen.cxx
@@ -1,5 +1,7 @@
 #include "WireCellIface/IWireParameters.h"
 
+#include "WireCellUtil/Singleton.h"
+#include "WireCellUtil/PluginManager.h"
 #include "WireCellUtil/NamedFactory.h"
 #include "WireCellUtil/Testing.h"
 
@@ -11,29 +13,11 @@ using namespace WireCell;
 
 int main()
 {
-    void* library = dlopen("libWireCellGen.so", RTLD_NOW);
-    Assert(library);
+    PluginManager& pm = PluginManager::instance();
+    Plugin* gen_plug = pm.add("WireCellGen");
+    Assert(gen_plug);
+
     
-    void* thing = dlsym(library, "force_link_WireParams");
-    Assert(thing);
-
-    int* number = reinterpret_cast<int*>(thing);
-    cerr << "Number is " << *number << endl;
-
-    void* reg_fun_ptr = dlsym(library, "register_WireParams_Factory");
-    Assert(reg_fun_ptr);
-
-    cerr << "Registering WireParams" << endl;
-    typedef void (*registration_function)();
-    registration_function regfun = reinterpret_cast<registration_function>(reg_fun_ptr);
-    regfun();
-
-    // void* see_ptr = dlsym(library, "see_WireParams_force_link");
-    // Assert(see_ptr);
-    // typedef int (*see_force_link)();
-    // see_force_link see_func = reinterpret_cast<see_force_link>(see_ptr);
-    // cerr << see_func() << endl;
-
     typedef NamedFactoryRegistry<IWireParameters> IWPFactoryRegistry;
     IWPFactoryRegistry* iwp1 = &Singleton< IWPFactoryRegistry >::Instance();
     IWPFactoryRegistry* iwp2 = &Singleton< IWPFactoryRegistry >::Instance();
